ccbe340fb930d28d2a3a256f094efca32a63b4f6
AimbotParce <marcparcerisa@gmail.com>
2024-04-06 23:20:35 +0200

    Pack to waitress

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 """
 Remember to set the environment variables:
+- API_PORT
+- API_HOST
 - DATABASE_URL
 - USERS_DATABASE_URL
 """
+
+import os
 
 from api import app
 from flask_cors import CORS
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config["CORS_HEADERS"] = "Content-Type"
+
+if __name__ == "__main__":
+    import waitress
+
+    waitress.serve(app, port=os.environ.get("API_PORT"), host=os.environ.get("API_HOST"))
