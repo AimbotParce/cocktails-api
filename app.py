b1b1fc5849c6aacb3883f631588d0b7251c29b6c
AimbotParce <marcparcerisa@gmail.com>
2024-04-06 14:04:38 +0200

    Use uvicorn

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 """
 Remember to set the environment variables:
-- API_PORT
-- API_HOST
 - DATABASE_URL
 - USERS_DATABASE_URL
 """
 
-import os
-
+from api import app
 from flask_cors import CORS
 
-from api import app
-
-if __name__ == "__main__":
-    import waitress
-
-    cors = CORS(app, resources={r"/*": {"origins": "*"}})
-    app.config["CORS_HEADERS"] = "Content-Type"
-
-    waitress.serve(app, port=os.environ.get("API_PORT"), host=os.environ.get("API_HOST"))
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config["CORS_HEADERS"] = "Content-Type"
